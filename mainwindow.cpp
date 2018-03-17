464d3b513582f540446ca51a9b16a23111199582
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2018-03-17 15:29:33 +0100

    Added x axis label

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -137,6 +137,11 @@ void MainWindow::on_timeout()
 
     // Check if the current row contains column names
     if (row_counter-1 == headerrow) {
+        // X-axis label
+        if (x_axis_column < row.length())
+            ui->plot->xAxis->setLabel(row[x_axis_column]);
+
+        // Legend labels
         for (int i = 0; i < columns.length(); i++) {
             DataVector *column = columns[i];
             if (column->column_index > row.length()-1)
